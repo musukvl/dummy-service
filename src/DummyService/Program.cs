3ad32e280eef94bf350c5d98ccd3384cf3acd70b
Alexey Ryazhskikh <musukvl@gmail.com>
2023-06-19 12:30:15 +0300

    Capture all type of requests

--- src/DummyService/Program.cs
+++ src/DummyService/Program.cs
@@ -2,8 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddCommandLine(args);
 var app = builder.Build();
 
-app.MapGet("{**path}", async (HttpContext context, string path) => {
-    await context.Response.WriteAsync($"Hello on port {context.Connection.LocalPort} path: {path}");
+app.MapFallback(async context => {
+    await context.Response.WriteAsync($"Hello on port {context.Connection.LocalPort} path: {context.Request.Path}");
 });
  
 app.Run();
